820aa1c5eab429ffd273b5bedfd742c460810f1b
bhadra-k <bhadra050127@gmail.com>
2025-08-23 10:45:28 +0530

    emoji converted text

--- emoji.py
+++ emoji.py
@@ -1,15 +1,32 @@
+# emoji={'happy':'😁','sad':'😢','angry':'😡','pizza':'🍕'}
+# n=input("message:")
+# if 'happy' in n:
+#     n=n.replace('happy',emoji['happy'])
+#     print(n)
+# elif 'sad' in n:
+#     n=n.replace('sad',emoji['sad'])
+#     print(n)
+# elif 'angry' in n:
+#     n=n.replace('angry',emoji['angry'])
+#     print(n)
+# else:    
+#     n=n.replace('pizza',emoji['pizza'])
+#     print(n)
+#     exit
+
+#OR
+
+# emoji={'happy':'😁','sad':'😢','angry':'😡','pizza':'🍕'}
+# n=input("Enter message:")
+# for key,value in emoji.items():
+#     n=n.replace(key,value)
+# print(n)
+
+#OR
+
 emoji={'happy':'😁','sad':'😢','angry':'😡','pizza':'🍕'}
-n=input("message:")
-if 'happy' in n:
-    n=n.replace('happy',emoji['happy'])
-    print(n)
-elif 'sad' in n:
-    n=n.replace('sad',emoji['sad'])
-    print(n)
-elif 'angry' in n:
-    n=n.replace('angry',emoji['angry'])
-    print(n)
-else:    
-    n=n.replace('pizza',emoji['pizza'])
-    print(n)
-    exit
+n=input("Enter message:")
+for a in emoji:
+    if a in n:
+        n=n.replace(a,emoji[a])
+print(n)
