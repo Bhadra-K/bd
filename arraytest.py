b9a9f12fb89012575c55d160e1a4b55c33746a95
bhadra-k <bhadra050127@gmail.com>
2025-08-23 12:05:54 +0530

    numpy operations

--- arraytest.py
+++ arraytest.py
@@ -1,7 +1,19 @@
 import numpy as np
+#zero ands ones array
 a=np.array([1,2,3])
 print(a)
 print(np.zeros((2,3)))
 print(np.ones((2,2)))
+#range 
 a=np.arange(0,10,2)
 print(a)
+#reshape
+a=np.arange(6)
+b=a.reshape((2,3))
+print("Reshaped:",b)
+#mean,median,standard deviation 
+a=np.array([1,2,3,4,])
+print(a)
+print(np.mean(a))
+print(np.median(a))
+print(np.std(a))
