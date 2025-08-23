a03a0b2b842b6abc6a8cbd48505f5bd95453a67a
bhadra-k <bhadra050127@gmail.com>
2025-08-23 11:03:07 +0530

    movie sets

---
+++ grocery.py
@@ -0,0 +1,7 @@
+Create a grocery billing system using lists.
+Have two lists: items = ["milk","bread","egg"] and prices = [40,25,5].
+Ask user to enter what items they bought and quantity.
+Calculate total bill.
+Apply discount:
+10% if total > 500
+20% if total > 1000
--- set.py
+++ set.py
@@ -1,3 +1,13 @@
+# Set Project – Movie Recommendation System
+# Problem:
+# Create a simple movie recommendation system using sets.
+# User A’s watched movies (set1)
+# User B’s watched movies (set2)
+# Program should show:
+# Movies both have watched (intersection)
+# Movies unique to User A (difference)
+# Suggested movies for User A based on User B’s watched list (set2 – set1)
+
 usera={'Inception','Matrix','Avatar','Titanic'} 
 userb={'Inception','Matrix','Jurassic Park','Avengers'}
 commonmovies=usera & userb
