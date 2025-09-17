b83afd18c7c2936533bc6dcb3161c2029a35cacb
bhadra-k <bhadra050127@gmail.com>
2025-09-17 16:42:36 +0530

    deep learning

---
+++ deep.py
@@ -0,0 +1,11 @@
+#CNNIMAGEPROCESSING
+# #1. import libraries
+import tensorflow as tf
+from tensorflow.keras import layers,models
+import matplotlib.pyplot as plt
+#2. load dataset(MNIST is a built in data)
+(x_train,y_train),(x_test,y_test)=tf.keras.datasets.mnist.load_data()
+print((x_train,y_train),(x_test,y_test))
+#3. normalize data(scale pixel values to 0-1)
+x_train=x_train.astype("float32") / 255.0
+x_test=x_test.astype("float32") / 255.0
