ce61aee7fc30f696b0f9dcffa19955d4818006d9
bhadra-k <bhadra050127@gmail.com>
2025-09-17 16:57:03 +0530

    deep learning

--- deep.py
+++ deep.py
@@ -1,5 +1,5 @@
 #CNNIMAGEPROCESSING
-# #1. import libraries
+#1. import libraries
 import tensorflow as tf
 from tensorflow.keras import layers,models
 import matplotlib.pyplot as plt
@@ -9,3 +9,6 @@ print((x_train,y_train),(x_test,y_test))
 #3. normalize data(scale pixel values to 0-1)
 x_train=x_train.astype("float32") / 255.0
 x_test=x_test.astype("float32") / 255.0
+#4. reshape data(CNN expects 3D: height,width,channels)
+x_train=x_train.reshape(-1,28,28,1)     #-1 means "figure this dimension out automatically"
+x_test=x_test.reshape(-1,28,28,1)       #1 is the no of channels
